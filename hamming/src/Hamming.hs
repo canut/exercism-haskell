a4975ce1ec19b4e32398fcb7dc3731b3e610db05
canut <michelevski@gmail.com>
2019-04-22 09:09:40 +0200

    solve hamming v2

--- hamming/src/Hamming.hs
+++ hamming/src/Hamming.hs
@@ -1,10 +1,6 @@
 module Hamming (distance) where
 
-import Control.Monad
-
 distance :: String -> String -> Maybe Int
-distance [] [] = Just 0
 distance xs ys
     | length xs /= length ys = Nothing
-    | head xs == head ys     = distance (tail xs) (tail ys)
-    | otherwise              = fmap sum $ sequence [Just 1, distance (tail xs) (tail ys)]
+    | otherwise              = Just . length . filter id $ zipWith (/=) xs ys
